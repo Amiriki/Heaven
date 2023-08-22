8b03d7fb640759c8afa2cec4af42fce6a076b40e
Amiriki <73649897+Amiriki@users.noreply.github.com>
2023-08-22 14:01:08 +0100

    Update api.lua

--- assets/api.lua
+++ assets/api.lua
@@ -32,15 +32,14 @@ local ValuableGems = {[32] = true, [33] = true, [34] = true, [35] = true, [36] =
 -- [[ Config Script ]] --
 getgenv().FOHAPI = {}
 FOHAPI.Configuration = {
-    ['AutofarmEnabled'] = false,
-    ['GeneralWeapon'] = "",
-    ['NPCWeapon'] = "",
-    ['AutofarmMap'] = "Savannah",
-    ['WebhookEnabled'] = false,
-    ['WebhookURL'] = "",
-    ['DisableOnJoin'] = true,
-    ['EnableOnLeave'] = true,
-    ['IgnorePlayers'] = false,
+    ['AutoGemResponseEnabled'] = false,
+    ['GemResponseMithril'] = "",
+    ['GemResponseDemonite'] = "",
+    ['GemResponseFury'] = "",
+    ['GemResponseDragon'] = "",
+    ['GemResponseSpirit'] = "",
+    ['GemResponseTitan'] = "",
+    ['GemResponseHallowed'] = "",
     ['AutoMapvoteRogue'] = true,
     ['GemESPEnabled'] = true,
     ['GemESPColour'] = Color3.fromRGB(255, 255, 255),
@@ -57,54 +56,9 @@ FOHAPI.Configuration = {
     ['DisableDecoration'] = true,
 }
 
--- [[ Autofarm Variables ]] --
-local PlayerThumbnail = game:HttpGet("https://thumbnails.roblox.com/v1/users/avatar?userIds="..LocalPlayer.UserId.."&size=420x420&format=Png&isCircular=false")
-local FailsafeTripped = false
-local Stats = LocalPlayer.leaderstats
-local Gold = Stats.Gold.Value
-local XP = Stats.XP.Value
-local GoldEarned = 0
-local ExperienceEarned = 0
-local HourlyGold = 0
-local HourlyExperience = 0
-local TimeElapsed = 0
-
 -- [[ Functions ]] --
 function NotifyChat(message, colour)
 	StarterGui:SetCore("ChatMakeSystemMessage", {Text = "[Field of Heaven] "..message, Color = colour, Font = Enum.Font.SourceSansBold, TextSize = 16})
-end
-
-function ObtainTargets()
-	local TargetsList
-    local EnemyTeam
-    local Index
-	local General
-
-	if LocalPlayer.Team == Teams:FindFirstChild('Neutral') then return {} end
-	if LocalPlayer.Team == Teams:FindFirstChild('Orc') then EnemyTeam = 'Human' end
-	if LocalPlayer.Team == Teams:FindFirstChild('Human') then EnemyTeam = 'Orc' end
-
-	TargetsList = NPCs[EnemyTeam]:GetChildren()
-
-	for i, npc in pairs(TargetsList) do
-		if npc.Name:find('General')  then
-			Index = i
-			General = npc
-		end
-	end
-
-    if FOHAPI.Configuration.IgnorePlayers then
-        for i, player in pairs(Players:GetPlayers()) do
-            if table.find(TargetsList, player.Name) then
-                TargetsList[i] = nil
-            end
-        end
-    end
-
-	TargetsList[Index] = nil
-	table.insert(TargetsList, #TargetsList, General)
-
-	return TargetsList
 end
 
 function SendWebhook()
@@ -116,11 +70,13 @@ function Attack(target, weapon)
 	if LocalPlayer.Backpack:FindFirstChild(weapon) then LocalPlayer.Character.Humanoid:EquipTool(LocalPlayer.Backpack:FindFirstChild(weapon)) end
 
 	repeat
-		if not target:FindFirstChild('Torso') or FailsafeTripped then return end
-		LocalPlayer.Character:FindFirstChild(weapon):Activate()
-		LocalPlayer.Character.HumanoidRootPart.CFrame = target.Torso.CFrame * CFrame.new(0,0,3)
+		pcall(function()
+			if not target:FindFirstChild('Torso') then return end
+			LocalPlayer.Character:FindFirstChild(weapon):Activate()
+			LocalPlayer.Character.HumanoidRootPart.CFrame = target.Torso.CFrame * CFrame.new(0,0,3)
+		end)
 		task.wait(0.125)
-	until not target or not target:FindFirstChild('Humanoid') or target:FindFirstChild('Humanoid').Health == 0
+	until not target or not target:FindFirstChild('Humanoid') or target:FindFirstChild('Humanoid').Health == 0 or not LocalPlayer.Character or not LocalPlayer.Character:FindFirstChild('Humanoid') or LocalPlayer.Character:FindFirstChild('Humanoid').Health == 0
 end
 
 function DrawTracer(Object, Thickness, Colour, Enabled) 
@@ -206,39 +162,18 @@ end
 
 -- [[ Events ]] --
 
+--[[
 LocalPlayer.CharacterAdded:Connect(function()
-    if FOHAPI.Configuration.AutofarmEnabled then
-        local CurrentWeapon
-        local Enemies = ObtainTargets()
-        for index, npc in pairs(Enemies) do
-            if not FOHAPI.Configuration.AutofarmEnabled or FailsafeTripped then return print'autofarm is disabled or failsafe is tripped' end
-            if Teams:FindFirstChild('Neutral') and LocalPlayer.Team == Teams:FindFirstChild('Neutral') then return end
-
-            if npc.Name:find('General') then CurrentWeapon = FOHAPI.Configuration.GeneralWeapon else CurrentWeapon = FOHAPI.Configuration.NPCWeapon end 
-	        repeat task.wait() until LocalPlayer.Backpack:FindFirstChild(CurrentWeapon) or LocalPlayer.Character:FindFirstChild(CurrentWeapon)
-			Attack(npc, CurrentWeapon)
-            Enemies[index] = nil
-        end
-    end
+    
 end)
 
 Players.PlayerAdded:Connect(function(Player)
-    -- [[ Autofarm Failsafe ]] --
-    if FOHAPI.Configuration.DisableOnJoin and FOHAPI.Configuration.AutofarmEnabled and not FailsafeTripped then
-	    FailsafeTripped = true
-	    LocalPlayer.Character:BreakJoints()
-    end
+
 end)
 
 Players.PlayerRemoving:Connect(function()
-    -- [[ Autofarm Failsafe ]] --
-    if FOHAPI.Configuration.EnableOnLeave and FOHAPI.Configuration.AutofarmEnabled and FailsafeTripped then
-	    if #Players:GetPlayers() == 1 then
-		    FailsafeTripped = false
-		    LocalPlayer.Character:BreakJoints()
-	    end
-    end
-end)
+
+end)]]
 
 RunService.RenderStepped:Connect(function()
     -- [[ Fullbright code (skidded from IY but whatever) ]] --
@@ -250,9 +185,10 @@ RunService.RenderStepped:Connect(function()
         Lighting.OutdoorAmbient = Color3.fromRGB(128, 128, 128)
     end
 
-    (LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()):WaitForChild('Humanoid').WalkSpeed = FOHAPI.Configuration.Walkspeed;
-    LocalPlayer.Character:WaitForChild('Humanoid').JumpPower = FOHAPI.Configuration.Jumppower;
-    LocalPlayer.Character:WaitForChild('Humanoid').UseJumpPower = true;
+    -- Disabled on suspicion of it being detected.
+    --(LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()):WaitForChild('Humanoid').WalkSpeed = FOHAPI.Configuration.Walkspeed;
+    --LocalPlayer.Character:WaitForChild('Humanoid').JumpPower = FOHAPI.Configuration.Jumppower;
+    --LocalPlayer.Character:WaitForChild('Humanoid').UseJumpPower = true;
 end)
 
 Projectiles.ChildAdded:Connect(function(obj)
@@ -282,8 +218,8 @@ Projectiles.ChildAdded:Connect(function(obj)
                 if FOHAPI.Configuration.GemESPEnabled then
 				    DrawGemESP(obj, "GemESPColour", "GemESPEnabled")
 			    end
+
 			    if FOHAPI.Configuration.PathfindToGem then
-				    --task.wait(1)
 				    PathfindToObject(obj, LocalPlayer.Character)
 			    end
             end
@@ -296,14 +232,7 @@ NPCs.DescendantAdded:Connect(function(obj)
         local Humanoid = obj:WaitForChild('Humanoid', 3)
         if Humanoid then
             obj:WaitForChild('Humanoid').Died:Connect(function()
-                if FOHAPI.Configuration.AutofarmEnabled then
-                    Players:Chat(':mapvote '..(FOHAPI.Configuration.Map or 'Savannah'))
-                    if FOHAPI.Configuration.WebhookEnabled then 
-                        SendWebhook() 
-                    end
-                end
-                
-                if FOHAPI.Configuration.AutoMapvoteRogue and not FOHAPI.Configuration.AutofarmEnabled then
+                if FOHAPI.Configuration.AutoMapvoteRogue then
                     Players:Chat(':mapvote rogue')
                 end
             end)
@@ -311,30 +240,19 @@ NPCs.DescendantAdded:Connect(function(obj)
     end
 end)
 
-spawn(function()
-	while task.wait(1) do
-		if FOHAPI.Configuration.AutofarmEnabled and not FailsafeTripped then 
-		    GoldEarned = Stats.Gold.Value - Gold
-		    ExperienceEarned = Stats.XP.Value - XP
-		    TimeElapsed = TimeElapsed + 1
-		    HourlyGold = (GoldEarned / TimeElapsed) * 3600
-		    HourlyExperience = (ExperienceEarned / TimeElapsed) * 3600
-		end
-	end
-end)
-
 -- Thanks to Sown for helping me with this
 
+--[[ Disabled on suspicion of flagging - it also breaks flight lmao
 local OldIndex
 OldIndex = hookmetamethod(game, "__index", newcclosure(function(Self, Key)
     if not checkcaller() and Self:IsA('Humanoid') and Key == "WalkSpeed" then
-        return Humanoid.MaxSpeed.Value
+        return Humanoid.WalkSpeed
     elseif not checkcaller() and Self:IsA('Humanoid') and Key == "JumpPower" then
         return 50
     end
 
     return OldIndex(Self, Key)
-end))
+end))]]
 
 -- [[ Server Message Remote ]] --
 
@@ -344,12 +262,17 @@ ReplicatedStorage.Remote.ShowPlayerMessage.OnClientEvent:Connect(function(text, 
 	end
 
     if text:find(' found a ') then
-        if text:split(" ")[1] == LocalPlayer.Name then
-            
+        local split = text:split(" ")
+        if split[1] == LocalPlayer.Name then
+            if FOHAPI.Configuration['GemResponse'..split[4]] then
+                if FOHAPI.Configuration.AutoGemResponseEnabled then
+                    task.wait(math.random(1, 2.5))
+                    ChatRemote:FireServer(FOHAPI.Configuration['GemResponse'..split[4]], "All")
+                end
+            end
         end
     end
 end)
-
 
 -- [[ Final Code ]] --
 
