81e274052be6a1b5902baed6d222093369360368
Amiriki <73649897+Amiriki@users.noreply.github.com>
2023-08-22 11:19:15 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,10 @@
+--[[
+This is a BETA version of Field of Heaven
+Do not release this anywhere on V3rmillion, ScriptBlox or any other script site.
+I will release the script myself publicly when it's ready
+Written by Amiriki
+22/08/2023
+]]
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Amiriki/Heaven/main/assets/api.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Amiriki/Heaven/main/assets/frontend.lua'))()
